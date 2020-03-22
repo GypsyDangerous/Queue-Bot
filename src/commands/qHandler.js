e9c74419d46748154dd29671ebb6551adb1f0a94
David Snyder <davidgraygs4@gmail.com>
2020-03-22 18:56:18 -0400

    fix bugs

--- src/commands/qHandler.js
+++ src/commands/qHandler.js
@@ -19,7 +19,6 @@ let current
 const IncludesArray = (arr1, arr2) => arr1.some(v => arr2.indexOf(v) >= 0)
 
 
-
 class Function {
     constructor(func, description, usage, modOnly) {
         this.execute = modOnly ? (msg, { config }) => modWare(msg, config, func) : func
@@ -42,11 +41,11 @@ QueueMember.prototype.toString = function memberToString(){
 }
 
 
-const modWare = (msg, config, cb) => {
+const modWare = async (msg, config, cb) => {
     if(msg.member.permissions.any(config.ModPerms)){
         cb(msg, config)
     }else{
-        msg.reply("You don't have permission to use this command")
+        await msg.reply("You don't have permission to use this command")
     }
 }
 
@@ -54,7 +53,6 @@ const toggleTalk = (current, config) => {
     current.voice.setChannel(config.QID)
     setTimeout(() => current.voice.setChannel(config.QnAId), 200)
 }
-
 
 const startHandler = async msg =>{
     if (!qStatus) {
@@ -96,7 +94,7 @@ const nextHandler = async (msg, config) => {
 const joinHandler = async (msg, config) => {
     if (qStatus) {
         const member = new QueueMember(msg.member, config)
-        if (queue.includes(member)) {
+        if (queue.findIndex(current => current.id === member.id) >= 0) {
             return await msg.reply("You are already in the queue")
         } else {
             queue.push(member)
@@ -108,9 +106,10 @@ const joinHandler = async (msg, config) => {
 
 const statusHandler = async msg => {
     const id = msg.author.id
-    if (queue.includes(id)) {
-        await msg.reply(`Your position in the queue is ${queue.indexOf(id) + 1}`)
-    } else if (current.user.id === id) {
+    const index = queue.findIndex(member => member.id === id)
+    if (index >= 0) {
+        await msg.reply(`Your position in the queue is ${index + 1}`)
+    } else if (current && current.id === id) {
         await msg.reply("You are currently being served")
     } else (
         await msg.reply("You are not in the queue")
@@ -119,23 +118,26 @@ const statusHandler = async msg => {
 
 const resetHandler = async msg => {
     queue = []
-    msg.channel.send("The Queue has been Cleared, Queueing is still enabled")
+    msg.channel.send(`The Queue has been cleared, Queueing is still ${qStatus ? "enabled" : "disabled"}`)
 }
 
-const memberHandler = async msg => {
-    msg.channel.send(`the current queue members are [${queue.join(", ")}], queuing is ${qStatus ? "enabled" : "disabled"}`)
+const memberHandler = async (msg, config) => {
+    await msg.channel.send(`the current queue members are [${queue.slice(0, config.displayLimit).join(", ")}${queue.length > config.displayLimit ? " + " + (queue.length - config.displayLimit) : "" }], queueing is ${qStatus ? "enabled" : "disabled"}`)
 }
 
 const priorityHandler = async (msg, config) => {
     if(!qStatus){
-        queue.sort(member => member.isPriority ? -1 : 1)
-        msg.channel.send(`Queue priority filter complete the queue is [${queue.join(", ")}]`)
+        const priorityQueue = queue.filter(member => member.isPriority)
+        const regularQueue = queue.filter(member => !member.isPriority)
+        queue = [...priorityQueue, ...regularQueue]
+        await msg.channel.send(`Queue priority filter complete`)
+        await memberHandler(msg, config)
     }else{
-        msg.channel.send("Queue priority filter cannot be done while Queuing is enabled")
+        await msg.channel.send("Queue priority filter cannot be done while Queueing is enabled")
     }
 }
 
-const help = (msg, {args, config, functions}) => {
+const help = async (msg, {args, config, functions}) => {
     if(args.length === 0){
         const available = (msg.member.permissions.any(config.ModPerms)) ? Object.keys(functions) : Object.keys(functions).filter(key => !functions[key].modOnly)
         const embed = new MessageEmbed()
@@ -144,7 +146,7 @@ const help = (msg, {args, config, functions}) => {
             .addField("Commands", `The available commands are \`${available.join("`, `")}\``)
             .addField("Tips", ` - Some commands are moderator only\n - Run \`${config.prefix}q help <command>\` to get help on a specific command`)
             .setFooter("Queue Bot help")
-        msg.channel.send(embed)
+        await msg.channel.send(embed)
     }else{
         const command = args.join(" ")
         const func = functions[command]
@@ -155,27 +157,36 @@ const help = (msg, {args, config, functions}) => {
                 .addField("usage", config.prefix+func.usage, true)
                 .addField("Moderator only", func.modOnly.toString(), true)
                 .setFooter("Queue Bot help")
-            msg.channel.send(embed)
+            await msg.channel.send(embed)
         }
+    }
+}
+
+const leaveHandler = async (msg) => {
+    if(queue.findIndex(member => member.id === msg.member.id) >= 0){
+        queue = queue.filter(member => member.id !== msg.member.id)
+        await msg.reply("You have been removed from the queue")
+    }else{
+        await msg.reply("You are not currently in the queue")
     }
 }
 
 
 const functions = {
-    "start": new Function(startHandler, "Enables, queuing until stopped", "q start", true),
-    "stop": new Function(stopHandler, "Disables queuing until started", "q stop", true),
+    "start": new Function(startHandler, "Enables, queueing until stopped", "q start", true),
+    "stop": new Function(stopHandler, "Disables queueing until started", "q stop", true),
     "next": new Function(nextHandler, "goes to the next user in the queue", "q next", true),
     "reset": new Function(resetHandler, "clears the queue", "q reset", true),
-    "members": new Function(memberHandler, "returns a list of all the people in the queue and the queuing status", "q members", true),
+    "members": new Function(memberHandler, "returns a list of all the people in the queue and the queueing status", "q members", true),
     "priority": new Function(priorityHandler, "filters priority roles to the front of the queue 'Incomplete'", "q priority", true),
     "join": new Function(joinHandler, "join the queue if you are not already in it", "q join", false),
     "status": new Function(statusHandler, "returns your current position in the queue", "q status", false),
+    "leave": new Function(leaveHandler, "removes the sender from the queue", "q leave", false),
     "help": new Function(help, "sends help messages for the general bot or specific commands", "q help", false)
 }
 
 functions.position = functions.status
-
-
+functions.clear = functions.reset
 
 module.exports = async (msg, {args, config}) => {
     const command = args.shift()
